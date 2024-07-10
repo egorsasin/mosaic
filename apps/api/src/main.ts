4a393407df7c0f9ebf256a56cc7bb7faf8bada0e
Egor Sasin <egorsasin@gmail.com>
2024-07-10 16:14:33 +0200

    modify config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -21,7 +21,7 @@ import {
   getEntitiesFromPlugins,
 } from '@mosaic/core/plugin';
 
-import { appConfig } from 'config/api.config';
+import { appConfig } from './config/mosaic-config';
 
 async function runPluginConfigurations(
   config: RuntimeConfig
