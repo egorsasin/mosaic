5324c4b445fddc70aa3594cee6753dd430d63ef6
Egor Sasin <egorsasin@gmail.com>
2024-07-10 16:06:35 +0200

    update config

---
+++ config/api.config.ts
@@ -0,0 +1,13 @@
+import { API_PORT } from '@mosaic/common';
+import { MosaicConfig } from '@mosaic/core/config';
+import { examplePaymentHandler } from './payment';
+
+export const appConfig: MosaicConfig = {
+  apiOptions: {
+    port: API_PORT,
+    cors: {},
+  },
+  paymentOptions: {
+    paymentMethodHandlers: [examplePaymentHandler],
+  },
+};
