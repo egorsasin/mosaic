6ab0471ad477038c932f5048340b4ded3d333b0d
Egor Sasin <egorsasin@gmail.com>
2024-09-17 13:04:51 +0200

    fix config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
 import { MosCoreModule } from '@mosaic/core/core.module';
 
@@ -7,7 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MosCoreModule, NestConfigModule.forRoot()],
+  imports: [MosCoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
